0fd64141619240db2db865d53117c7c34c98f065
donnieSky <qmhs414@gmail.com>
2020-01-16 18:25:59 +0800

    Remove unused code.

--- app/src/main/java/me/donnie/reader/detail/NewsDetailViewModel.kt
+++ app/src/main/java/me/donnie/reader/detail/NewsDetailViewModel.kt
@@ -9,9 +9,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.donnie.reader.data.entities.NewsDetail
 import me.donnie.reader.data.entities.Result
-import me.donnie.reader.utils.appCtx
 import org.jsoup.Jsoup
-import timber.log.Timber
 
 class NewsDetailViewModel : ViewModel() {
   
@@ -19,23 +17,14 @@ class NewsDetailViewModel : ViewModel() {
   val result: LiveData<Result<NewsDetail>> = _result
   
   private fun parseLink(url: String): Result<NewsDetail> {
-    val assetManager = appCtx.resources.assets
     return try {
-      val inputStream = assetManager.open("webview/html/reader.html")
-      val template = Jsoup.parse(inputStream, null, "")
-      
       val document = Jsoup.connect(url).get()
       val title = document.title()
       val img = document.select("img").first()
       val imgSrc = img.absUrl("src")
-  
-      /*val content = template.getElementById("content")
-      content.attr("style", "visibility: visible;")*/
       val container = document.getElementsByClass("container").html()
-      //val html = content.append(container).html()
       
-      //Timber.d("content html: $container <><><><><><")
-      Result.Success(NewsDetail(title = title, img = imgSrc, content = container, html = template.html()))
+      Result.Success(NewsDetail(title = title, img = imgSrc, content = container))
     } catch (e: Exception) {
       e.printStackTrace()
       Result.Error(e)
